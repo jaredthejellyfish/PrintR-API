5f55cbbe399de3ecf3552513faefaba3e5a44b54
Gerard Hernandez <ger.almenara@gmail.com>
2023-02-27 14:13:04 +0000

    I take back the CORS thing

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins='https://printer.jaredthejelly.me',
     allow_credentials=True,
-    allow_methods=["GET"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
